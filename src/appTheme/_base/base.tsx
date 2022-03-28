c9ef8dca9c623a5910efce6db84626c3cffe7951
DarlinCondC <darlinapraez@condorcoin.co>
2022-03-28 11:37:43 -0500

    Linear gradient in background

--- src/appTheme/_base/base.tsx
+++ src/appTheme/_base/base.tsx
@@ -1,21 +1,19 @@
-import {
-    StyleSheet,
-    Platform,
-    Dimensions,
-} from "react-native";
+import { StyleSheet, Platform, Dimensions } from "react-native";
 
 const windowWidth = Dimensions.get("screen").width;
 const windowHeight = Dimensions.get("window").height;
 export const stylesB = StyleSheet.create({
-    //NO TOCAR PACHITO
-    body: {
-        height: windowHeight,
-        width: windowWidth,
-        flex: 1,
-        backgroundColor: "#000",
-    },
-    completo: {
-        alignItems: "center",
-        flex: 1,
-    },
+  //NO TOCAR PACHITO
+  body: {
+    height: windowHeight,
+    width: windowWidth,
+    flex: 2,
+  },
+  linear: {
+    flex: 1,
+  },
+  completo: {
+    alignItems: "center",
+    flex: 1,
+  },
 });
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -8,17 +8,25 @@ import {
 import { View, Text, SafeAreaView } from "react-native";
 import BarStatus from "../components/BarStatus";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Login = () => {
   return (
-    <SafeAreaView style={stylesB.body}>
-      <BarStatus />
-      <View style={stylesB.completo}>
-        <Text style={[stylesM.textColorWhite, stylesM.fontSizeSixteen]}>
-          Login
-        </Text>
-      </View>
-    </SafeAreaView>
+    <LinearGradient
+      colors={["#38AA35", "#1F5326"]}
+      style={stylesB.linear}
+      start={{ x: 0, y: 1.6 }}
+      end={{ x: 0, y: 0 }}
+    >
+      <SafeAreaView style={stylesB.body}>
+        <BarStatus />
+        <View style={stylesB.completo}>
+          <Text style={[stylesM.textColorWhite, stylesM.fontSizeSixteen]}>
+            Login
+          </Text>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
