abcf18a4be936f19e2e00332aa235f927c805b41
Jcano <juancano57@hotmail.com>
2022-04-22 10:02:04 -0500

    adding functions the balance of token

--- controller/index.ts
+++ controller/index.ts
@@ -1,4 +1,7 @@
 import * as solanaWeb3 from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { PublicKey } from '@solana/web3.js';
+
 
 import { fetch as fetchPolyfill } from 'whatwg-fetch'
 global.fetch = fetchPolyfill
@@ -8,6 +11,15 @@ global.fetch = fetchPolyfill
 ////  obtener el balance y transferir SOL y SPL Tokens  ////
 ////////////////////////////////////////////////////////////
 
+//variables
+const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new solanaWeb3.PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL")
+const LAMPORTS_PER_SOL = solanaWeb3.LAMPORTS_PER_SOL 
+
+//crear conexion
+function createConnection(cluster:string) {
+    return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl(cluster))
+}
+
 //Create keypair
 async function createAccount() {
     const response = await fetch('Aqui va tu direccion')
@@ -15,6 +27,37 @@ async function createAccount() {
     return json
 }
 
+//buscar cuentas asociadas a tokens
+async function findAssociatedTokenAddress(
+    walletAddress: PublicKey,
+    tokenMintAddress: PublicKey
+  ): Promise<PublicKey> {
+    return (
+      await solanaWeb3.PublicKey.findProgramAddress(
+        [
+          walletAddress.toBuffer(),
+          TOKEN_PROGRAM_ID.toBuffer(),
+          tokenMintAddress.toBuffer(),
+        ],
+        SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+      )
+    )[0];
+}
+
+//obtener balance del token ECOPUNTO
+async function getToken(publicKey: string, splToken: string){
+    const connection = createConnection("mainnet-beta")
+    const account = await findAssociatedTokenAddress(new PublicKey(publicKey), new PublicKey(splToken))
+
+  try {
+    const balance = await connection.getTokenAccountBalance(new PublicKey(account.toString()))
+    return balance.value.uiAmount
+  } catch (e) {
+    return 0
+  }
+}
+
 export {
-    createAccount
+    createAccount,
+    getToken
 }
